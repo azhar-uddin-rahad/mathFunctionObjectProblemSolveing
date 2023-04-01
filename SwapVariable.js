84cd5366c060dd4f1590eb0ec328ff440e663277
azharrahad31 <azherudinrahad@gmail.com>
2023-04-02 02:50:16 +0600

    swap whit variable and destructing

---
+++ SwapVariable.js
@@ -0,0 +1,13 @@
+//Swap using variable 
+let num1=20;
+let num2=30;
+/* let temp=num1;
+num1=num2;
+num2=temp;
+console.log(num1,num2); */
+
+
+//another system that's call a destructing
+[num1,num2]=[num2,num1];
+
+console.log(num1,num2);
