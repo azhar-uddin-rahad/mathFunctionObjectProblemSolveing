2ec083267cd61be20523c15491265b2feab38ecd
azharrahad31 <azherudinrahad@gmail.com>
2023-04-02 00:10:43 +0600

    Math abs

--- mathobjectFunction.js
+++ mathobjectFunction.js
@@ -1,4 +1,16 @@
 
-
+//Math.pow using for set to the power on number
 const toThePower=Math.pow(2,4)
-console.log(toThePower);
+//console.log(toThePower);
+//outPut: 16
+
+
+// math.abs return the absolute  Number 
+const a =20;
+const b=30;
+
+const result=Math.abs(a-b);
+console.log(result);
+//outpt:10
+
+
